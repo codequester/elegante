46ad854872b3fd6c144ff8d996e0768dffbb7604
Shankar Govindarajan <shgovind@redhat.com>
2018-12-05 10:42:10 -0600

    Added the shutdown hook

--- src/main/java/com/sample/elegante/LongRunningRequestController.java
+++ src/main/java/com/sample/elegante/LongRunningRequestController.java
@@ -7,12 +7,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 class LongRunningRequestController {
     @RequestMapping(value="/runlong", method=RequestMethod.GET)
-
 	String testGraceness() throws InterruptedException {
 		for(int counter=1; counter<=60; counter++) {
             Thread.sleep(100);
             System.out.println("Control Thread Running for thread [" + Thread.currentThread().getName() + "]. . ." + (counter) + "-Times");
 		}
 		return "We need to See This";
-	}
+    }
 }
--- src/main/java/com/sample/elegante/ShutDownHookRegister.java
+++ src/main/java/com/sample/elegante/ShutDownHookRegister.java
@@ -17,13 +17,18 @@ class ShutDownHookRegister {
     }
 
     private void registerShutDownHook(Thread controlThread) {
-    //     System.out.println("Registering the shutdown Hook for Thread-["+controlThread.getName()+"]");
-	// 	Thread monitorThread = new Thread(() -> {
-	// 		customHealthIndicator.setIsShutDownTriggered(true);
-	// 		System.out.println("Shutdown Sequence INITIATED -->");
-	// 		controlThread.interrupt();
-	// 		System.out.println("Controll Thread - ["+controlThread.getName()+"] Stopped before program exit. . .");
-	// 	});
-	// 	Runtime.getRuntime().addShutdownHook(monitorThread);
+        System.out.println("Registering the shutdown Hook for Thread-["+controlThread.getName()+"]");
+		Thread monitorThread = new Thread(() -> {
+			//customHealthIndicator.setIsShutDownTriggered(true);
+            System.out.println("Shutdown Sequence INITIATED -->");
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+			controlThread.interrupt();
+			System.out.println("Controll Thread - ["+controlThread.getName()+"] Stopped before program exit. . .");
+		});
+		Runtime.getRuntime().addShutdownHook(monitorThread);
 	 }
 }
